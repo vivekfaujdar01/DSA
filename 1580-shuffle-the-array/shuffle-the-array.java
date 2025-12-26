edcf67669b9837f3dedb5e05cd061c20adc7ea15
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2025-12-26 23:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (57.47%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,21 +1,14 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] res = new int[nums.length];
-        int[] even = new int[n];
-        int[] odd = new int[n];
-        int j=0;
-
-        for(int i=0;i<n;i++){
-            even[i] = nums[i];
-        }
-
-        for(int i=n;i<2*n;i++){
-            odd[i-n] = nums[i];
-        }
-        j=0;
-        for(int i=0;i<n;i++){
-            res[j++] = even[i];
-            res[j++] = odd[i];
+        int x = 0;
+        int y = n;
+        
+        for(int i=0;i<nums.length;i+=2){
+            res[i] = nums[x];
+            res[i+1] = nums[y];
+            x++;
+            y++;
         }
 
         return res;
