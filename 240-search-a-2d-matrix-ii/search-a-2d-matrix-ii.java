a780bfa407821b0d85faf46c6e16ddd49cd77cc6
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-02-03 22:52:15 +0530

    Time: 3 ms (87.33%) | Memory: 48.3 MB (39.10%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null
+        ) {
+            return false;
+        }
+        int col = matrix[0].length-1;
+        int row = 0;
+        while(col >= 0 && row <= matrix.length-1) {
+            if(target == matrix[row][col]) {
+                return true;
+            } else if(target < matrix[row][col]) {
+                col--;
+            } else if(target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+    }
+}
