c0a3926e7ae9f31006d00f3a4dc321180bf63d59
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-02-05 18:07:47 +0530

    Time: 0 ms (100.00%) | Memory: 49.8 MB (10.92%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // we will calculate left height and right height of a tree 
+    // if both are equal then we can directly calculate nodes by formula 2^h-1
+    // if they are not then recursively we will calculate the nodes
+    // time complexity:- O(logn * logn) = O(log^2 n)
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+
+        if (leftHeight == rightHeight) {
+            return (1 << leftHeight) - 1;   // 2^h-1 formula for total number of nodes in a perfect binary tree.
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    private int getLeftHeight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.left;
+        }
+        return height;
+    }
+    private int getRightHeight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.right;
+        }
+        return height;
+    }
+}
