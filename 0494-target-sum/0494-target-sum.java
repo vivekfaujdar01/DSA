ffccfbf8fa5f11d12c3fd04e6bfa1a59cd70d339
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-03-09 16:58:10 +0530

    Time: 202 ms (33.66%), Space: 43.2 MB (66.63%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,25 +1,46 @@
 class Solution {
+    // converting this question to partition into two subsets whose difference equal to target
     // recursion approach
+    // sum(P) - sum(N) = target
+    // sum(P) + sum(N) = totalSum
+    // 2 * sum(P) = target + totalSum
+    // sum(P) = (target + totalSum) / 2
+    // 416. Partition Equal Subset Sum once check this to understand more this almost similar
     public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
-        return solve(n - 1, target, nums);
+
+        int totalSum = 0;
+        for(int num : nums){
+            totalSum += num;
+        }    
+
+        if((target + totalSum) % 2 != 0 || Math.abs(target) > totalSum){
+            return 0;
+        }         
+
+        int s = (target + totalSum) / 2;
+
+        return countSubsets(nums.length - 1, s, nums);
     }
 
-    private int solve(int index, int target, int[] nums) {
+    private int countSubsets(int index, int sum, int[] nums) {
 
         if(index == 0) {
-            int ways = 0;
-
-            if(target == nums[0]) ways++;
-            if(target == -nums[0]) ways++;
-
-            return ways;
+            if(sum == 0 && nums[0] == 0){
+                return 2;
+            }
+            if(sum == 0 || sum == nums[0]){
+                return 1;
+            }
+            return 0;
         }
 
-        int plus = solve(index - 1, target - nums[index], nums);
+        int notTake = countSubsets(index - 1, sum, nums);
 
-        int minus = solve(index - 1, target + nums[index], nums);
-
-        return plus + minus;
+        int take = 0;
+        if(nums[index] <= sum){
+            take = countSubsets(index - 1, sum - nums[index], nums);
+        }
+            
+        return take + notTake;
     }
-} 
+}
