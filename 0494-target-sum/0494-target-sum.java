0389f5f7a49e5cf0772516d355d52d1edb0012bd
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-03-09 17:01:12 +0530

    Time: 5 ms (79.35%), Space: 45.3 MB (35.85%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,46 +1,46 @@
 class Solution {
-    // converting this question to partition into two subsets whose difference equal to target
-    // recursion approach
-    // sum(P) - sum(N) = target
-    // sum(P) + sum(N) = totalSum
-    // 2 * sum(P) = target + totalSum
-    // sum(P) = (target + totalSum) / 2
-    // 416. Partition Equal Subset Sum once check this to understand more this almost similar
+    // tabualtion approach
     public int findTargetSumWays(int[] nums, int target) {
 
         int totalSum = 0;
-        for(int num : nums){
+        for(int num : nums)
             totalSum += num;
-        }    
 
         if((target + totalSum) % 2 != 0 || Math.abs(target) > totalSum){
             return 0;
-        }         
+        }       
 
-        int s = (target + totalSum) / 2;
+        int S = (target + totalSum) / 2;
 
-        return countSubsets(nums.length - 1, s, nums);
-    }
+        int n = nums.length;
 
-    private int countSubsets(int index, int sum, int[] nums) {
+        int[][] dp = new int[n][S + 1];
 
-        if(index == 0) {
-            if(sum == 0 && nums[0] == 0){
-                return 2;
-            }
-            if(sum == 0 || sum == nums[0]){
-                return 1;
-            }
-            return 0;
+        // Base cases
+        if(nums[0] == 0){
+            dp[0][0] = 2;
+        }
+        else{
+            dp[0][0] = 1;
+        }   
+
+        if(nums[0] != 0 && nums[0] <= S){
+            dp[0][nums[0]] = 1;
         }
 
-        int notTake = countSubsets(index - 1, sum, nums);
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j <= S; j++) {
 
-        int take = 0;
-        if(nums[index] <= sum){
-            take = countSubsets(index - 1, sum - nums[index], nums);
+                int notTake = dp[i-1][j];
+                int take = 0;
+                if(nums[i] <= j){
+                    take = dp[i-1][j - nums[i]];
+                }
+
+                dp[i][j] = take + notTake;
+            }
         }
-            
-        return take + notTake;
+
+        return dp[n-1][S];
     }
 }
