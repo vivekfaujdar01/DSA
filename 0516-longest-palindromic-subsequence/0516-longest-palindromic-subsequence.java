4dcce2f4bd9b101e3b34df9097aa0e58123edf44
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-03-10 19:13:47 +0530

    Time: 30 ms (80.49%), Space: 64.6 MB (7.42%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,25 +1,36 @@
 class Solution {
-    // Longest Palindromic Subsequence = Longest Common Subsequence(s, reverse(s))
-    // 1143. Longest Common Subsequence just a trick to solve it and almost same logic
+    // memoisation approach to solve it without lcs
     public int longestPalindromeSubseq(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
+        
         int n = s.length();
-
-        int[][] dp = new int[n+1][n+1];
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-
-                if(s.charAt(i-1) == rev.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-
+        int[][] dp = new int[n][n];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j] = -1;
             }
         }
+        
+        return solve(s,0,n-1,dp);
+    }
 
-        return dp[n][n];
+    private int solve(String s,int i,int j,int[][] dp){
+        
+        if(i > j) return 0;
+        
+        if(i == j) return 1;
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+              
+        if(s.charAt(i) == s.charAt(j)){
+            dp[i][j] = 2 + solve(s,i+1,j-1,dp);
+        }
+        else{
+            dp[i][j] = Math.max(solve(s,i+1,j,dp), solve(s,i,j-1,dp));
+        }
+        
+        return dp[i][j];
     }
 }
