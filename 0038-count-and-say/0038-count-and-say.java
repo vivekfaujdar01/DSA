42fb7612ddd70dc8b2e07775cd98ed9cd1d9a4c2
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-03-17 22:33:02 +0530

    Time: 2 ms (99.6%), Space: 42.9 MB (90.25%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String countAndSay(int n) {
+        String res = "1";
+        for (int i = 1; i < n; i++) {
+            res = buildNext(res);
+        }
+        return res;
+    }
+
+    private String buildNext(String s) {
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                result.append(count);
+                result.append(s.charAt(i - 1));
+                count = 1;
+            }
+        }
+        result.append(count);
+        result.append(s.charAt(s.length() - 1));
+        return result.toString();
+    }
+}
