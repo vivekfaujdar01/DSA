4fabb54ad8d9bf6e9910137a58b199161b008ac8
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-03-24 18:13:39 +0530

    Time: 6 ms (66.39%), Space: 65.5 MB (77.04%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,49 +1,27 @@
 class Solution {
-    // TABULATION method to solve it
+    // Space optimization with 2 arrays
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
 
-        int[][] dp = new int[n + 1][2];
-
-        // dp[n][0] = dp[n][1] = 0
+        int[] ahead = new int[2]; // dp[i+1]
+        int[] curr = new int[2];  // dp[i]
 
         for (int i = n - 1; i >= 0; i--) {
 
             // BUY state
-            int buy = -prices[i] + dp[i + 1][0];
-            int skipBuy = dp[i + 1][1];
-            dp[i][1] = Math.max(buy, skipBuy);
+            int buy = -prices[i] + ahead[0]; // perform buy
+            int skipBuy = ahead[1];          // skip buying
+            curr[1] = Math.max(buy, skipBuy);
 
             // SELL state
-            int sell = prices[i] - fee + dp[i + 1][1]; // subtracting fee while selling
-            int skipSell = dp[i + 1][0];
-            dp[i][0] = Math.max(sell, skipSell);
+            int sell = prices[i] - fee + ahead[1]; // perform sell (fee applied)
+            int skipSell = ahead[0];               // skip selling
+            curr[0] = Math.max(sell, skipSell);
+
+            // move current to ahead
+            ahead = curr;
         }
 
-        return dp[0][1]; // start with buy allowed
+        return ahead[1]; // start from buy state
     }
 }
-
-// MEMOIZATION
-// int solve(int i, int buy, int[] prices, int fee, int[][] dp) {
-//     if (i == prices.length)
-//         return 0;
-   
-//     if (dp[i][buy] != -1){   // already computed
-//         return dp[i][buy];
-//     }
-        
-//     int profit;
-
-//     if (buy == 1) {
-//         int take = -prices[i] + solve(i + 1, 0, prices, fee, dp);
-//         int skip = solve(i + 1, 1, prices, fee, dp);
-//         profit = Math.max(take, skip);
-//     } else {
-//         int take = prices[i] - fee + solve(i + 1, 1, prices, fee, dp);
-//         int skip = solve(i + 1, 0, prices, fee, dp);
-//         profit = Math.max(take, skip);
-//     }
-
-//     return dp[i][buy] = profit; // store result
-// }
