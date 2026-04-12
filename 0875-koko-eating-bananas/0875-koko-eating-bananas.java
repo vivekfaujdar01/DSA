f71d4bec3d6375da7b0ec61b1b2d6d996839ee47
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-04-12 10:23:26 +0530

    Time: 11 ms (57.93%), Space: 48.3 MB (15.91%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,24 @@
+// to understand it https://leetcode.com/problems/koko-eating-bananas/solutions/1703687/javac-a-very-very-well-detailed-explanat-act8/
+class Solution {
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 1000000000;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(canEatInTime(piles, mid, h)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
+    }
+    public boolean canEatInTime(int piles[], int k, int h){
+        long hours = 0;
+        for(int pile : piles){
+            int div = pile / k;
+            hours += div;
+            if(pile % k != 0) hours++;
+        }
+        return hours <= h;
+    }
+}
