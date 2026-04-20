8702d921c005f6dc1ee152d5d33307ada07db106
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-04-20 22:48:22 +0530

    Time: 454 ms (18.75%), Space: 0B (100%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+# ASC for ascending order and DESC for descending order
+SELECT DISTINCT author_id as id from Views WHERE author_id = viewer_id ORDER BY ID ASC;
