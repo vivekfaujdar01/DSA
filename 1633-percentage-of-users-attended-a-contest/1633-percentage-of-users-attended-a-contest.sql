9a67a6d603e12c45bc95e425250f241206f5388e
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-04-21 22:00:46 +0530

    Time: 1125 ms (16.74%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,5 @@
+SELECT r.contest_id,
+ROUND(100.0 * COUNT(r.user_id) / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, contest_id ASC
