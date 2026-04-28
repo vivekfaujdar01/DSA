d0f1d2adef8b45b0b40f3fcdd9deb661c33a717f
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-04-28 22:59:37 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.68%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int tribonacci(int n) {
+        int dp[] = new int[n+1];
+
+        for(int i=0;i<=n;i++){
+            dp[i] = -1;
+        }
+
+        return rec(n, dp);
+    }
+    public int rec(int n, int[] dp){
+        if(n == 0){
+            return 0;
+        }    
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 1;
+        }
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = rec(n-1, dp) + rec(n-2, dp) + rec(n-3, dp);
+    }
+}
