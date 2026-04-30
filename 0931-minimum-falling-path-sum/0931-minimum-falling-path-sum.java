3040a5a0e1d327bafc1a3a24515389c3a9c44211
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-04-30 14:38:24 +0530

    Time: 9 ms (16.73%), Space: 47 MB (80.41%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,53 +1,42 @@
 class Solution {
-    // Bottom-Up Tabulation
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int[][] dp = new int[n][n];
+        int m = matrix[0].length;
 
-        for (int col = 0; col < n; col++) {
-            dp[0][col] = matrix[0][col];
+        int[][] dp = new int[n][m];
+
+        for(int j = 0; j < m; j++){
+            dp[0][j] = matrix[0][j];
         }
 
-        for (int row = 1; row < n; row++) {
-            for (int col = 0; col < n; col++) {
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < m; j++){
 
-                int up = matrix[row][col] + dp[row - 1][col];
-                
-                int leftDiag = col > 0 ? matrix[row][col] + dp[row - 1][col - 1] : (int)1e9;
-                int rightDiag = col < n - 1 ? matrix[row][col] + dp[row - 1][col + 1] : (int)1e9;
+                int up = matrix[i][j] + dp[i-1][j];
 
-                dp[row][col] =  Math.min(up, Math.min(leftDiag, rightDiag));
+                int left = matrix[i][j];
+                if(j > 0){
+                    left += dp[i-1][j-1];
+                } else {
+                    left += (int)1e9;
+                }
+
+                int right = matrix[i][j];
+                if(j < m-1){
+                    right += dp[i-1][j+1];
+                } else {
+                    right += (int)1e9;
+                }
+
+                dp[i][j] = Math.min(up, Math.min(left, right));
             }
         }
 
-        int ans = Integer.MAX_VALUE;
-        for (int col = 0; col < n; col++) {
-            ans = Math.min(ans, dp[n - 1][col]);
+        int min = Integer.MAX_VALUE;
+        for(int j = 0; j < m; j++){
+            min = Math.min(min, dp[n-1][j]);
         }
 
-        return ans;
+        return min;
     }
 }
-    // Recursive Code (Brute Force)
-    // public int minFallingPathSum(int[][] matrix) {
-    //     int n = matrix.length;
-    //     int ans = Integer.MAX_VALUE;
-    //     
-    //     for (int col = 0; col < n; col++) {
-    //         ans = Math.min(ans, dfs(matrix, n - 1, col));
-    //     }
-    //     
-    //     return ans;
-    // }
-
-    // private int dfs(int[][] matrix, int row, int col) {
-    //     if (col < 0 || col >= matrix.length)
-    //         return (int)1e9;  // invalid path
-    //     
-    //     if (row == 0)
-    //         return matrix[0][col];
-    //     int upLeft = matrix[row][col] +  dfs(matrix, row - 1, col - 1);
-    //     int up = matrix[row][col] +  dfs(matrix, row - 1, col);
-    //     int upRight = matrix[row][col] +  dfs(matrix, row - 1, col + 1);
-    //     return Math.min(up, Math.min(upLeft, upRight));
-    // }
