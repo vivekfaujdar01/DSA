6a8f267b949f4f31e56288bebc16d849f4b63630
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-05-02 18:48:42 +0530

    Time: 0 ms (100%), Space: 43.6 MB (34.05%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -22,12 +22,17 @@ class Solution {
     }
     
     private boolean isMirror(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) {
-            return true;
-        }
+        // if (node1 == null && node2 == null) {
+        //     return true;
+        // }
+        // if (node1 == null && node2 == null) {
+        //     return false;
+        // }
+
         if (node1 == null || node2 == null) {
-            return false;
+            return node1 == node2;
         }
+        
         return node1.val == node2.val && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
     }
 }
