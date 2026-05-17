d9fe646ffcb19a9f71e548dbcef134f7feff3a55
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-05-17 17:44:10 +0530

    Time: 45 ms (90.23%), Space: 54.1 MB (69.07%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    // return (await promise1) + (await promise2);
+
+    return Promise.all([promise1, promise2]).then(([res1, res2]) => res1+res2);
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
