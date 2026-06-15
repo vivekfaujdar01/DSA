5f2627499248107cc5f5731efd23c87867d3e42f
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-06-15 22:14:43 +0530

    Time: 58 ms (11.84%), Space: 54.3 MB (9.78%) - LeetHub

--- 2622-cache-with-time-limit/2622-cache-with-time-limit.js
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.js
@@ -1,23 +1,57 @@
 var TimeLimitedCache = function() {
-    map = new Map();
-    var timer;
+    this.cache = {};
 };
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
 
 TimeLimitedCache.prototype.set = function(key, value, duration) {
-    let res = map.has(key);
-    if(res) {
-        clearTimeout(map.get(key).timer);
+    const exists = key in this.cache;
+
+    if (exists) {
+        clearTimeout(this.cache[key].timeoutId);
     }
-    timer = setTimeout(()=>map.delete(key), duration);
-    map.set(key, {value: value, timer: timer} );
-    return res;
+
+    const timeoutId = setTimeout(() => {
+        delete this.cache[key];
+    }, duration);
+
+    this.cache[key] = {
+        value,
+        timeoutId
+    };
+
+    return exists;
 };
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
 
 TimeLimitedCache.prototype.get = function(key) {
-    if(map.has(key)) return map.get(key).value;
-    return -1;
+    if (!(key in this.cache)) {
+        return -1;
+    }
+
+    return this.cache[key].value;
 };
 
+/** 
+ * @return {number} count of non-expired keys
+ */
+ 
 TimeLimitedCache.prototype.count = function() {
-    return map.size;
+    return Object.keys(this.cache).length;
 };
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
