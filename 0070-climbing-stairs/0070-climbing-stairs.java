f49f19ec306cd2840d8d5115e7595e4b6bec849d
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-06-24 10:10:58 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
 
-        for(int i=0;i<=n;i++){
-            dp[i] = -1;
-        }
-        
-        return rec(n, dp);
+        return rec(n - 1, dp);
     }
-    public int rec(int n, int[] dp){
-        if(n == 0 || n == 1){
-            return 1;
-        }    
 
-        if(dp[n] != -1) return dp[n];
+    private int rec(int i, int[] dp) {
+        if (i == 0 || i == 1){ // dp[0] is storing result for stair 1 & dp[1] is storing result for stair 2;
+            return dp[i] = i+1;
+        }
 
-        return dp[n] = rec(n-1, dp) + rec(n-2, dp);
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        return dp[i] = rec(i - 1, dp) + rec(i - 2, dp);
     }
 }
