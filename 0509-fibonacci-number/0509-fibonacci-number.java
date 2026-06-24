0908496a0b9adcd1c07f73cb5d8ad9705551cc25
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-06-24 11:24:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,25 +1,40 @@
 class Solution {
+    
     public int fib(int n) {
-        int dp[] = new int[n+1];
-
-        for(int i=0;i<=n;i++){
-            dp[i] = -1;
-        }
-        
-        return rec(n, dp);
-    }
-
-    public int rec(int n, int[] dp){
         if(n == 0){
             return 0;
-        }    
-        if(n == 1){
-            return 1;
         }
 
-        if(dp[n] != -1) return dp[n];
+        int prev2 = 0;
+        int prev1 = 1;
+        
+        for(int i=2;i<=n;i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
 
-        return dp[n] = rec(n-1, dp) + rec(n-2, dp);
+        return prev1;
     }
-    
 }
+// Tabulation (bottom - up)
+
+// int[] dp = new int[n+1];
+// Arrays.fill(dp, -1);
+// dp[0] = 0;
+// dp[1] = 1;
+// for(int i=2;i<=n;i++){
+//     dp[i] = dp[i-1] + dp[i-2];
+// }
+// return dp[n];
+
+// Memoization (top - down)
+// public int rec(int n, int[] dp){
+//     if(n<=1){
+//         return n;
+//     } 
+//     if(dp[n] != -1){
+//         return dp[n];
+//     }
+//     return dp[n] = fib(n-1) + fib(n-2);
+// }
