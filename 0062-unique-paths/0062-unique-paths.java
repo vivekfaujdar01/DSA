f7fb8f4cbaa98c4eba180b87ecd26f1e5b8bd70f
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-06-25 10:59:32 +0530

    Time: 0 ms (100%), Space: 41.8 MB (91.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,53 @@
+//                     tc            sc
+// Pure Recursion -> O(2^(m+n)) -> O(m+n)
+// Memoization    -> O(m × n)   -> O(m × n) + recursion stack
+// Tabulation     -> O(m × n)   -> O(m × n)
+// Space Optimized-> O(m × n)   -> O(n)
+
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        return solve(m - 1, n - 1, dp);
-    }
+    public int uniquePaths(int m, int n){
+        int[] prev = new int[n];
 
-    private int solve(int i, int j, int[][] dp) {
-        if (i == 0 && j == 0) return 1;
-    
-        if (i < 0 || j < 0) return 0;
+        for(int i = 0; i < m; i++){
 
-        if (dp[i][j] != 0) return dp[i][j];
+            int[] curr = new int[n];
 
-        int up = solve(i - 1, j, dp);
-        int left = solve(i, j - 1, dp);
-        return dp[i][j] = up + left;
+            for(int j = 0; j < n; j++){
+
+                if(i == 0 && j == 0){
+                    curr[j] = 1;
+                }    
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i > 0)   up = prev[j];
+                    if(j > 0)   left = curr[j-1];
+                    curr[j] = up + left;
+                }
+            }
+
+            prev = curr;
+        }
+
+        return prev[n-1];
     }
 }
+
+// tabulation approach
+
+// public int uniquePaths(int m, int n){
+//     int dp[m][n];
+//     for(int i = 0; i < m; i++){
+//         for(int j = 0; j < n; j++){
+//             if(i == 0 && j == 0)    dp[i][j] = 1;
+//             else{
+//                 int up = 0;
+//                 int left = 0;
+//                 if(i > 0)   up = dp[i-1][j];
+//                 if(j > 0)   left = dp[i][j-1];
+//                 dp[i][j] = up + left;
+//             }
+//         }
+//     }
+//     return dp[m-1][n-1];
+// }
