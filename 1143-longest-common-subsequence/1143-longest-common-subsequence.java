ece90bea2d683b20aa576787d4e718421e960820
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-06-29 14:05:48 +0530

    Time: 20 ms (78.39%), Space: 46.8 MB (92.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,27 +1,74 @@
 class Solution {
-    // space optimised
+    // Space Optimization
     public int longestCommonSubsequence(String text1, String text2) {
 
         int n = text1.length();
         int m = text2.length();
 
-        int[] prev = new int[m+1];
-        int[] curr = new int[m+1];
+        int[] prev = new int[m];
 
-        for(int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
+            int[] curr = new int[m];
 
-            for(int j = 1; j <= m; j++) {
+            for (int j = 0; j < m; j++) {
 
-                if(text1.charAt(i-1) == text2.charAt(j-1))
-                    curr[j] = 1 + prev[j-1];
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    curr[j] = 1;
+                    if (i > 0 && j > 0) {
+                        curr[j] += prev[j - 1];
+                    }
+                } else {
+                    int a = 0;
+                    int b = 0;
+                    if (i > 0) {
+                        a = prev[j];
+                    }
+                    if (j > 0) {
+                        b = curr[j - 1];
+                    }
+                    curr[j] = Math.max(a, b);
+                }
 
-                else
-                    curr[j] = Math.max(prev[j], curr[j-1]);
             }
 
-            prev = curr.clone();
+            prev = curr;
         }
 
-        return prev[m];
+        return prev[m - 1];
     }
 }
+
+//  Tabulation
+
+// public int longestCommonSubsequence(String text1, String text2) {
+
+//     int n = text1.length();
+//     int m = text2.length();
+
+//     int[][] dp = new int[n][m];
+
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < m; j++) {
+
+//             if (text1.charAt(i) == text2.charAt(j)) {
+//                 dp[i][j] = 1;
+//                 if (i > 0 && j > 0) {
+//                     dp[i][j] += dp[i - 1][j - 1];
+//                 }
+//             } else {
+//                 int a = 0;
+//                 int b = 0;
+//                 if (i > 0) {
+//                     a = dp[i - 1][j];
+//                 }
+//                 if (j > 0) {
+//                     b = dp[i][j - 1];
+//                 }
+//                 dp[i][j] = Math.max(a, b);
+//             }
+
+//         }
+//     }
+
+//     return dp[n - 1][m - 1];
+// }
