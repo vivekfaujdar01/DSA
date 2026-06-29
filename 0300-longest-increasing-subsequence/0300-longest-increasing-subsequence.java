3e7aeb53d1003d911abac1c03404f03b41bca8d1
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-06-29 21:24:14 +0530

    Time: 427 ms (5%), Space: 237.4 MB (7.14%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,50 @@
+class Solution {
+    // shifting index by +1 because prev = -1
+    public int lengthOfLIS(int[] arr) {
+        int n = arr.length;
+        int[][] dp = new int[n][n + 1];
+
+        for (int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+
+        return solve(0, -1, arr, dp);
+    }
+
+    public int solve(int i, int prev, int[] arr, int[][] dp) {
+        if (i == arr.length)
+            return 0;
+
+        if (dp[i][prev + 1] != -1)
+            return dp[i][prev + 1];
+
+        int notTake = solve(i + 1, prev, arr, dp);
+
+        int take = 0;
+        if (prev == -1 || arr[i] > arr[prev]) {
+            take = 1 + solve(i + 1, i, arr, dp);
+        }
+
+        return dp[i][prev + 1] = Math.max(take, notTake);
+    }
+}
+
+// recursive code
+
+// public int lengthOfLIS(int[] arr) {
+//     return solve(0, -1, arr);
+// }
+
+// public int solve(int i, int prev, int[] arr) {
+//     if (i == arr.length)
+//         return 0;
+
+//     int notTake = solve(i + 1, prev, arr);
+
+//     int take = 0;
+//     if (prev == -1 || arr[i] > arr[prev]) {
+//         take = 1 + solve(i + 1, i, arr);
+//     }
+
+//     return Math.max(take, notTake);
+// }
