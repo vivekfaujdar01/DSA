4c0ebe802f9c379238cd701bf3a5c2b69e250a3e
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-07-07 18:30:31 +0530

    Time: 1 ms (97.65%), Space: 42.2 MB (95.29%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,23 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1)
-            return x;
-
+        
         int start = 1;
         int end = x;
-        int mid = -1;
+        int ans = 0;
 
         while (start <= end) {
-            mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2;
 
-            if ((long) mid * mid > (long) x)
-                end = mid - 1;
-            else if (mid * mid == x)
+            if ((long) mid * mid == x) {
                 return mid;
-            else
-                start = mid + 1;
+            } else if ((long) mid * mid < x) {
+                ans = mid;        
+                start = mid + 1;  
+            } else {
+                end = mid - 1;
+            }
         }
 
-        return (int)end;
+        return ans;
     }
 }
