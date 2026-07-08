e51d22373cf6b6263038156304a757f5bf5cb5e9
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-07-08 21:21:24 +0530

    Time: 0 ms (100%), Space: 46.6 MB (94.83%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        // find the middle element of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // create a new node with the value of the middle element
+        TreeNode root = new TreeNode(slow.val);
+        // recursively convert the left half of the linked list to the left subtree of the root
+        if (prev != null) {
+            prev.next = null;
+            root.left = sortedListToBST(head);
+        }
+        // recursively convert the right half of the linked list to the right subtree of the root
+        root.right = sortedListToBST(slow.next);
+        return root;  
+    }
+}
