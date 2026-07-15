6a9cd15b0d363f79ca94ae5b755cbb772206a540
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-07-15 18:52:52 +0530

    Time: 0 ms (100%), Space: 42.6 MB (39.85%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,6 @@
+class Solution {
+    // https://leetcode.com/problems/gcd-of-odd-and-even-sums/solutions/8397713/math-full-explanation-video-solution-ava-qaz9
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
