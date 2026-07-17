5e1c8e9f102f7e5d6576a27515dba0e9dad7ea97
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-07-17 21:14:43 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.61%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,20 @@
 class Solution {
     // recursion
     public int fib(int n) {
-        return rec(n);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return rec(n, dp);
     }
-    int rec(int n){
+    int rec(int n, int[] dp){
         // base cond.
         if(n==0 || n==1){
             return n;
         }
 
-        // main logic
+        if(dp[n] != -1){
+            return dp[n];
+        } 
 
-        return rec(n-1) + rec(n-2);
+        return dp[n] = rec(n-1, dp) + rec(n-2, dp);
     }
 }
