7940eda05b319fbcf5146068f36498274f631f96
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-07-17 21:23:24 +0530

    Time: 0 ms (100%), Space: 41.5 MB (98.37%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,20 +1,30 @@
 class Solution {
-    // recursion
+    // Tabulation
     public int fib(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-        return rec(n, dp);
-    }
-    int rec(int n, int[] dp){
-        // base cond.
-        if(n==0 || n==1){
+        if(n<=1){
             return n;
         }
+        dp[0] = 0;
+        dp[1] = 1;
 
-        if(dp[n] != -1){
-            return dp[n];
-        } 
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
 
-        return dp[n] = rec(n-1, dp) + rec(n-2, dp);
+        return dp[n];
     }
+    // int rec(int n, int[] dp){
+    //     // base cond.
+    //     if(n==0 || n==1){
+    //         return n;
+    //     }
+
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     } 
+
+    //     return dp[n] = rec(n-1, dp) + rec(n-2, dp);
+    // }
 }
