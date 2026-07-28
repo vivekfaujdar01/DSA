86d3c32af2fe67ccc2eede802c7c6842443b4363
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-07-28 07:48:49 +0530

    Time: 1 ms (86.79%), Space: 44.1 MB (57.71%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,27 +1,25 @@
 class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> subset = new ArrayList<>();
+
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        rec(0, nums, new ArrayList<>(), ans);
-
-        return ans;
+        backtrack(0, nums);
+        return result;
     }
 
-    void rec(int index, int[] nums, List<Integer> current, List<List<Integer>> ans) {
+    private void backtrack(int index, int[] nums) {
+        // Store the current subset
+        result.add(new ArrayList<>(subset));
 
-        if (index == nums.length) {
-            ans.add(new ArrayList<>(current));
-            return;
+        for (int i = index; i < nums.length; i++) {
+            // Choose
+            subset.add(nums[i]);
+
+            // Recurse
+            backtrack(i + 1, nums);
+
+            // Remove (Backtrack)
+            subset.remove(subset.size() - 1);
         }
-
-        // Take
-        current.add(nums[index]);
-        rec(index + 1, nums, current, ans);
-
-        // Backtrack
-        current.remove(current.size() - 1);
-
-        // Don't Take
-        rec(index + 1, nums, current, ans);
     }
 }
