afb1bea888e24ccfe12adbafc43b1c6069822535
Vivek Faujdar <152777809+vivekfaujdar01@users.noreply.github.com>
2026-08-01 19:27:14 +0530

    Time: 147 ms (20.04%), Space: 43.2 MB (59.33%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean makesquare(int[] nums) {
+        int k = 4;
+        int sum = 0;
+        
+        for (int num : nums) sum += num;
+
+        if (sum % k != 0) return false;
+
+        int target = sum / k;
+
+        Arrays.sort(nums);
+
+        boolean[] used = new boolean[nums.length];
+
+        return dfs(nums, used, k, 0, 0, target);
+    }
+    boolean dfs(int[] nums, boolean[] used, int k, int start, int currentSum, int target) {
+
+        if (k == 1)
+            return true;
+
+        if (currentSum == target)
+            return dfs(nums, used, k - 1, 0, 0, target);
+
+        for (int i = start; i < nums.length; i++) {
+
+            if (used[i]) continue;
+
+            if (currentSum + nums[i] > target) continue;
+
+            used[i] = true;
+
+            if (dfs(nums, used, k, i + 1, currentSum + nums[i], target)) return true;
+
+            used[i] = false;
+        }
+
+        return false;
+    }
+}
